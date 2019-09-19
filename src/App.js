5beffe8ee6a590651092a03448e382e422d43cfb
Binyam Tadele <binyadis@gmail.com>
2019-09-19 16:01:54 +0300

    portfolio 1.2

--- src/App.js
+++ src/App.js
@@ -1,60 +1,61 @@
-import React, { Component } from 'react';
-import Navigation from './components/desktop-components/Navigation/Navigation';
-import Footer from './components/desktop-components/Footer/Footer';
-import MobVersion from './AppMob';
-import './App.css';
-
+import React, { Component } from "react";
+import Navigation from "./components/desktop-components/Navigation/Navigation";
+import Footer from "./components/desktop-components/Footer/Footer";
+import "./App.css";
 
 const initialState = {
-	width: window.innerWidth,
-}
+  width: window.innerWidth
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = initialState;
+  }
 
-	constructor() {
-	  super();
-	  this.state = initialState;
-	}
+  _isMounted = false;
 
-	_isMounted = false;
+  componentWillMount() {
+    window.addEventListener("resize", this.handleWindowSizeChange);
+  }
 
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleWindowSizeChange);
+    this._isMounted = false;
+  }
 
-	componentWillMount() {
-	  window.addEventListener('resize', this.handleWindowSizeChange);
-	}
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
 
-	componentWillUnmount() {
-	  window.removeEventListener('resize', this.handleWindowSizeChange);
-	  this._isMounted = false;
-	}
+  componentDidMount() {
+    this._isMounted = true;
+  }
 
-	handleWindowSizeChange = () => {
-	  this.setState({ width: window.innerWidth });
-	};
-
-	componentDidMount(){
-		this._isMounted = true;
-	}
-
-
-	render() {
-	  const { width } = this.state;
-	  const isMobile = width <= 500;
-	  if (isMobile) {
-	    return (
-	      <div>
-			<MobVersion />
-		  </div>
-	    );
-	  } else {
-	    return (
-			<div className="App">
-			  <Navigation />			 
-			  <Footer />
-			</div>
-	    );
-	  }
-	}
+  render() {
+    const { width } = this.state;
+    const isMobile = width <= 500;
+    if (isMobile) {
+      // return (
+      //   <div>
+      // 	<MobVersion />
+      //   </div>
+      // );
+      return (
+        <div className="App">
+          <Navigation />
+          <Footer />
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <Navigation />
+          <Footer />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
